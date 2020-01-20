ffdb2bab12e1262e8ac47501ce70f5c1ddc0b3c0
Alfredo Castillon <fredoacmk@gmail.com>
2020-01-19 22:39:03 -0600

    init

---
+++ test/models/post_test.rb
@@ -0,0 +1,23 @@
+require 'test_helper'
+
+class PostTest < ActiveSupport::TestCase
+  test "should be invalid without a title" do
+    posts(:one).title = nil
+    assert_nil posts(:one).title
+    assert_equal false, posts(:one).valid?
+  end
+
+  test "should be invalid without a body" do
+    posts(:one).body = nil
+    assert_nil posts(:one).body
+    assert_equal false, posts(:one).valid?
+  end
+
+  test "post title should be a string" do
+    assert_equal true, posts(:one).title.is_a?(String)
+  end
+
+  test "post body should be a string" do
+    assert_equal true, posts(:one).body.is_a?(String)
+  end
+end
